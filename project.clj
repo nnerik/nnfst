cefca6321988b6626d659311f5db511b24a64a4b
Erik Haugan <erik@haugan.no>
2010-06-14 22:38:05 +0200

    Cleanup

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.nynorsk "1.0.0-SNAPSHOT"
+(defproject org.nynorsk "0.1.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
--- test/org/nynorsk/core_test.clj
+++ test/org/nynorsk/core_test.clj
@@ -1,5 +1,5 @@
-(ns org.nynorsk.core-test
-  (:use [org.nynorsk.core] :reload-all)
+(ns org.nynorsk.fst-test
+  (:use [org.nynorsk.fst] :reload-all)
   (:use [clojure.test]))
 
 (deftest replace-me ;; FIXME: write
