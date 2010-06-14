686da360ab3973306fa1378a0c3da58edd5682df
Erik Haugan <erik@haugan.no>
2010-06-14 23:35:03 +0200

    Project def

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject org.nynorsk "0.1.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject org.nynorsk/nnfst "0.1.0-SNAPSHOT"
+  :description "Finite state transducer library"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+  :namespaces [org.nynorsk.fst])
